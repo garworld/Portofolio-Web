7eb3bc04b4dbe884343eb69f86da0f24caea1181
Garasuryanegara <suryanegarasinatriyya@gmail.com>
2023-09-13 15:06:42 +0700

    update:useEffect theme

--- portofolio/src/App.js
+++ portofolio/src/App.js
@@ -6,10 +6,14 @@ import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import QuestionPage from "./pages/QuestionPage";
 import Navbar from "./components/Navbar";
+import { useEffect } from "react";
 
 AOS.init();
 
 function App() {
+  useEffect(() => {
+    localStorage.setItem("theme", "dark");
+  }, []);
   return (
     <>
       <div className="App">
